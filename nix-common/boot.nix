fc092991ee14df7c6bee8d28b59ba05368009774
Dor Zion <DorZion@users.noreply.github.com>
2023-09-04 08:35:25 +0300

    Removed left-over nix-common folder

--- nix-common/boot.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-
-{
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.supportedFilesystems =  [ "ntfs" ];
-}
