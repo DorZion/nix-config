aab19103bde5afb8fabf359fe360e2de000e6c6e
Dor Zion <DorZion@users.noreply.github.com>
2023-09-05 20:49:13 +0300

    Changed home/ to have default.nix

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.dor = import ./home/home.nix;
+            home-manager.users.dor = import ./home;
             home-manager.extraSpecialArgs = specialArgs;
           } 
         ];
