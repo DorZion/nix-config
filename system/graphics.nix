e58dd13de6f642b478d55c7593a7cd4bbaeadcb4
Dor Zion <DorZion@users.noreply.github.com>
2023-09-19 23:48:29 +0300

    Removed exposed ICD
    RE4 needs a dedicated fix, not the whole system

--- system/graphics.nix
+++ system/graphics.nix
@@ -28,7 +28,7 @@
      vulkan-tools
   ];
 
-  environment.variables = {
-    VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
-  };
+  # environment.variables = {
+  #   VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
+  # };
 }
